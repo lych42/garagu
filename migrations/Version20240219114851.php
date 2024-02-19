d37349d5462b0a815e63e043e60dfd25c175dccb
Charly Guerin <lannister135@gmail.com>
2024-02-19 12:54:27 +0100

    maj crud

--- migrations/Version20240219114851.php
+++ migrations/Version20240219114851.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240219110456 extends AbstractMigration
+final class Version20240219114851 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240219110456 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE horaires (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, jour VARCHAR(255) NOT NULL, heure_ouverture VARCHAR(255) NOT NULL, heure_fermeture VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE TABLE vehichle (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, brand VARCHAR(255) NOT NULL, model VARCHAR(255) NOT NULL, year VARCHAR(255) NOT NULL, mileage VARCHAR(255) NOT NULL)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE horaires');
+        $this->addSql('DROP TABLE vehichle');
     }
 }
