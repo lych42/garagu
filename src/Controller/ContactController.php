8b9987e704fa8974107c5f60ab6cbe1cd830a365
Charly Guerin <lannister135@gmail.com>
2024-02-19 13:58:15 +0100

    add contact controller

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\HorairesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(HorairesRepository $horairesRepository): Response
+    {
+        $horaires = $horairesRepository->findAll();
+
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+            'horaires' => $horaires
+        ]);
+    }
+}
