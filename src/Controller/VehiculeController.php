1b88ba4f6516180984d3e6aed583dc3a7cbbc179
Charly Guerin <lannister135@gmail.com>
2024-02-19 15:42:23 +0100

    maj vehichles

--- src/Controller/VehiculeController.php
+++ src/Controller/VehiculeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\VehichleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class VehiculeController extends AbstractController
 {
     #[Route('/vehicule', name: 'app_vehicule')]
-    public function index(): Response
+    public function index(VehichleRepository $vehicleRepository): Response
     {
+        $vehicles = $vehicleRepository->findAll();
+
         return $this->render('vehicule/index.html.twig', [
-            'controller_name' => 'VehiculeController',
+            'controller_name' => 'VehichuleController',
+            'vehicles' => $vehicles
         ]);
     }
 }
