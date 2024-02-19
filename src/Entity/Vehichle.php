179bb703709df7acf08ba02314e68b5a5efb5719
Charly Guerin <lannister135@gmail.com>
2024-02-19 13:37:17 +0100

    maj

--- src/Entity/Vehichle.php
+++ src/Entity/Vehichle.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\VehichleRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: VehichleRepository::class)]
@@ -25,6 +26,16 @@ class Vehichle
 
     #[ORM\Column(length: 255)]
     private ?string $mileage = null;
+
+    // NOTE : 
+    #[Vich\UploadableField(mapping: 'voitures_images', fileNameProperty: 'path', size: 'size')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $imageName = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
 
     public function getId(): ?int
     {
@@ -78,4 +89,33 @@ class Vehichle
 
         return $this;
     }
+
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageName(string $imageName): self
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
 }
