96f7a19b0f25a892b79728ef44f10980030924ae
Charly Guerin <lannister135@gmail.com>
2024-02-19 12:27:26 +0100

    shedule footer

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\HorairesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Horaires;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(HorairesRepository $horairesRepository): Response
     {
+        $horaires = $horairesRepository->findAll();
 
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
+            'horaires' => $horaires
         ]);
     }
 }
