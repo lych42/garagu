21208a716a4edf4c6a8b34a3d87606762a78c20e
Charly Guerin <lannister135@gmail.com>
2024-02-19 12:04:05 +0100

    add horaires

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,18 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\HorairesRepository;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(HorairesRepository $horairesRepository): Response
+    public function index(): Response
     {
-        $horaires = $horairesRepository->findAll();
 
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
-            'horaires' => $horaires
         ]);
     }
 }
