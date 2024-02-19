b96533e5757d499aaa34911bbde321ad0d0b195c
Charly Guerin <lannister135@gmail.com>
2024-02-19 14:37:38 +0100

    maj temp

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -6,17 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\HorairesRepository;
-
+use App\Repository\ServiceRepository;
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(HorairesRepository $horairesRepository): Response
+    public function index(HorairesRepository $horairesRepository, ServiceRepository $serviceRepository): Response
     {
         $horaires = $horairesRepository->findAll();
+        $services = $serviceRepository->findAll();
 
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
-            'horaires' => $horaires
+            'horaires' => $horaires,
+            'services' => $services
         ]);
     }
 }
